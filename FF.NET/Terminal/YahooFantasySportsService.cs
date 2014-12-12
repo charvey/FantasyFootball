b637c31c7fe5609b84209f232e9ad0b897c03895
Chris Harvey <chrischarvey@outlook.com>
2014-12-12 10:37:53 -0500

    Cleaned up references

--- FF.NET/Terminal/YahooFantasySportsService.cs
+++ FF.NET/Terminal/YahooFantasySportsService.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+﻿
 using DotNetOpenAuth.OAuth;
 using DotNetOpenAuth.Messaging;
 using DotNetOpenAuth.OAuth.ChannelElements;

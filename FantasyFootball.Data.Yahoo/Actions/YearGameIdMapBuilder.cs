ff4e39b43f4d5e81ccf0003349d65f0cb4b50d57
Chris Harvey <chrischarvey@outlook.com>
2015-08-09 15:35:50 -0400

    Added action to build list of Football GameIds by year

---
+++ FantasyFootball.Data.Yahoo/Actions/YearGameIdMapBuilder.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace FantasyFootball.Data.Yahoo.Actions
+{
+    public class YearGameIdMapBuilder
+    {
+        public void Build()
+        {
+            var map = new Dictionary<int, string>();
+            var x = new FantasySportsService();
+            foreach (var potentialId in Enumerable.Range(0, int.MaxValue))
+            {
+                if (map.ContainsKey(DateTime.Now.Year))
+                    break;
+                try
+                {
+                    var game = x.Game(potentialId.ToString());
+                    if (game == null)
+                        continue;
+                    if (game.name == "Football")
+                        map[game.season] = game.game_id;
+                }
+                catch (JsonException)
+                {
+                }
+            }
+            File.WriteAllLines("Year_GameId", map.OrderBy(kvp => kvp.Key).Select(kvp => kvp.Key + ":" + kvp.Value));
+        }
+    }
+}

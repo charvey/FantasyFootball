3a6b045b68df7c2af6daa9e9ea51d70063729612
Chris Harvey <chrischarvey@outlook.com>
2015-08-09 15:36:46 -0400

    Updated Yahoo Games calls

--- FantasyFootball.Data.Yahoo/FantasySportsService.cs
+++ FantasyFootball.Data.Yahoo/FantasySportsService.cs
@@ -2,6 +2,10 @@
 using System.Diagnostics;
 using FantasyFootball.Data.Yahoo.Models;
 using Newtonsoft.Json;
+using System.Linq;
+using System.IO;
+using FantasyFootball.Data.Yahoo.Actions;
+using System.Text.RegularExpressions;
 
 namespace FantasyFootball.Data.Yahoo
 {
@@ -9,20 +13,23 @@ namespace FantasyFootball.Data.Yahoo
     {
         private FantasySportsWebService webService = new FantasySportsWebService();
 
-        public IEnumerable<Game> Games
+        public IEnumerable<Game> Games()
         {
-            get
-            {
-                var json = webService.Games();
-                Debugger.Break();
-                return JsonConvert.DeserializeObject<WebServiceResponse>(json).fantasy_content.game;
-            }
+            if (!File.Exists("Year_GameId"))
+                new YearGameIdMapBuilder().Build();
+            var ids = File.ReadAllLines("Year_GameId").Select(x => x.Split(':')[1]);
+            return Games(ids.ToArray());
         }
 
-        public IEnumerable<Game> Game(string gameId)
+        public IEnumerable<Game> Games(params string[] gameIds)
+        {
+            return gameIds.Select(Game);
+        }
+
+        public Game Game(string gameId)
         {
             var json = webService.Game(gameId);
-            return JsonConvert.DeserializeObject<WebServiceResponse>(json).fantasy_content?.game;
+            return JsonConvert.DeserializeObject<WebServiceResponse>(json).fantasy_content?.game.Single();
         }
 
         public IEnumerable<Player> Players(string game_key)
--- FantasyFootball.Data.Yahoo/YahooWebService.cs
+++ FantasyFootball.Data.Yahoo/YahooWebService.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Net.Http;
 using System.Text;
-using System.Threading.Tasks;
 using Newtonsoft.Json;
 
 namespace FantasyFootball.Data.Yahoo

8e7f2e497b3ac6ba724504e73762d7c5f40a5abe
Chris Harvey <chrischarvey@outlook.com>
2015-08-09 15:55:40 -0400

    Emptied test console

--- FantasyFootball.Terminal/Program.cs
+++ FantasyFootball.Terminal/Program.cs
@@ -1,11 +1,5 @@
-﻿using FantasyFootball.Data.Yahoo;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Net.Http;
-using System.Text;
 
 namespace FantasyFootball.Terminal
 {
@@ -14,17 +8,6 @@ namespace FantasyFootball.Terminal
         public void Main(string[] args)
         {
             Console.WriteLine(Directory.GetCurrentDirectory());
-            
-            var x = new FantasySportsService();
-            foreach(var game in x.Games)
-            {
-                Console.WriteLine(game.name);
-                foreach(var player in x.Players(game.game_key))
-                {
-                    Console.WriteLine(player);
-                }
-            }
-            //Console.WriteLine(x.Players());
 
             Console.Read();
         }

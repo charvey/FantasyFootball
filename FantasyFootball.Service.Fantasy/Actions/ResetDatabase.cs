a843081ba8018a01dbbc7d2afe7590553b35052e
Chris Harvey <chrischarvey@outlook.com>
2015-08-09 15:37:17 -0400

    Created FantasyService ResetDatabase action

---
+++ FantasyFootball.Service.Fantasy/Actions/ResetDatabase.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+namespace FantasyFootball.Service.Fantasy.Actions
+{
+    public class ResetDatabase
+    {
+        public void Reset()
+        {
+            File.Delete(ConnectionString.Filename);
+            File.WriteAllBytes(ConnectionString.Filename, new byte[0]);
+            using (var fantasyContext = new FantasyContext())
+            {
+                fantasyContext.Database.EnsureCreated();
+            }
+        }
+    }
+}

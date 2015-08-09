25afc5bd386d5cdd529d88f5d975be92b4dd5b5b
Chris Harvey <chrischarvey@outlook.com>
2015-08-09 15:36:15 -0400

    Added simple time based Yahoo call throttling

--- FantasyFootball.Data.Yahoo/FantasySportsWebService.cs
+++ FantasyFootball.Data.Yahoo/FantasySportsWebService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace FantasyFootball.Data.Yahoo
@@ -10,16 +11,34 @@ namespace FantasyFootball.Data.Yahoo
     {
         private const string BaseUrl = "https://fantasysports.yahooapis.com/fantasy/v2";
 
+        private static DateTime WaitUntil = DateTime.MinValue;
+
         private Task<string> MakeCall(string url)
         {
+            while (DateTime.Now < WaitUntil)
+            {
+                var wait = WaitUntil.Subtract(DateTime.Now);
+                if (wait < TimeSpan.Zero)
+                    wait = TimeSpan.Zero;
+                Thread.Sleep(wait);
+            }
+            WaitUntil = DateTime.Now.AddSeconds(1);
+
             var request = new HttpRequestMessage(HttpMethod.Get, url + "?format=json");
             request.Headers.Add("Authorization", "Bearer " +AccessToken);
             return client.SendAsync(request).Result.Content.ReadAsStringAsync();
         }
 
-        public string Games()
+        public string Game(string gameId = "nfl")
         {
-            var url = BaseUrl + "/game/nfl";
+            var url = BaseUrl + "/game/" + gameId;
+
+            return MakeCall(url).Result;
+        }
+
+        public string Games(params string[] gameId)
+        {
+            var url = BaseUrl + "/games;game_keys=" + string.Join(",", gameId);
 
             return MakeCall(url).Result;
         }

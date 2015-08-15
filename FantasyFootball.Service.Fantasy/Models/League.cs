1dd7a3ecaee31a542a214e04ef31f9b2c17d4e69
Chris Harvey <chrischarvey@outlook.com>
2015-08-15 17:56:39 -0400

    Properly load League naviagation properties

--- FantasyFootball.Service.Fantasy/Models/League.cs
+++ FantasyFootball.Service.Fantasy/Models/League.cs
@@ -4,6 +4,14 @@ namespace FantasyFootball.Service.Fantasy.Models
 {
     public class League
     {
+        public League()
+        {
+            Teams = new HashSet<Team>();
+            Players = new HashSet<Player>();
+            DraftPicks = new HashSet<DraftPick>();
+            RosterPositions = new HashSet<RosterPosition>();
+        }
+
         public string Id { get; set; }
         public string Name { get; set; }
         public virtual ICollection<Team> Teams { get; set; }
--- FantasyFootball.Web/Controllers/LeagueController.cs
+++ FantasyFootball.Web/Controllers/LeagueController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Linq;
-using Microsoft.AspNet.Mvc;
 using FantasyFootball.Service.Fantasy;
+using FantasyFootball.Web.Models;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Data.Entity;
 
 namespace FantasyFootball.Web.Controllers
 {
@@ -23,9 +25,32 @@ namespace FantasyFootball.Web.Controllers
             }
         }
 
-        public IActionResult Draft(string leagueId)
+        public IActionResult Draft(string id)
         {
             return View();
+        }
+
+        public JsonResult DraftModel(string id)
+        {
+            using (var fantasyContext = new FantasyContext())
+            {
+                var league = fantasyContext.Leagues
+                    .Include(l => l.Teams)
+                    .Include(l => l.RosterPositions)
+                    .Single(l => l.Id == id);
+                var model = new DraftViewModel
+                {
+                    Teams = league.Teams.Select(t => t.Name),
+                    Rounds = Enumerable.Range(1, league.RosterPositions.Sum(rp=>rp.Count))
+                    .Select(r => league.Teams.ToDictionary(t => t.Id, t => (Player)null))
+                };
+                return Json(model);
+            }
+        }
+
+        public JsonResult UpdateDraft(string id, string teamId,string playerId,string round)
+        {
+            throw new System.Exception();
         }
     }
 }

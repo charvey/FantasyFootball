f45b5528dc2a12c8dd1f3dfa4058168b1527f8f8
Chris Harvey <chrischarvey@outlook.com>
2015-08-22 15:57:56 -0400

    Created simple player list to web project

---
+++ FantasyFootball.Web/Controllers/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.Mvc;
+using FantasyFootball.Service.Football;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FantasyFootball.Web.Controllers
+{
+    public class PlayerController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            using (var footballContext = new FootballContext())
+            {
+                return View(footballContext.Players);
+            }
+        }
+    }
+}

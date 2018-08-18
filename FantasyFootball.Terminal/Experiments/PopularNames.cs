8a27ef3b6b90751e062d7ec5fd5ee4d94b2d30df
Chris Harvey <chrischarvey@outlook.com>
2018-08-18 12:55:54 -0400

    Adds tools for listing popular names

---
+++ FantasyFootball.Terminal/Experiments/PopularNames.cs
@@ -0,0 +1,20 @@
+﻿using FantasyFootball.Data.Yahoo;
+using System.IO;
+using System.Linq;
+
+namespace FantasyFootball.Terminal.Experiments
+{
+    public static class PopularNames
+    {
+        public static void Analyze(FantasySportsService service, TextWriter output, string league_key)
+		{
+            var players = service.LeaguePlayers(league_key);
+            var firstNames = players.Select(p => p.name.first);
+            var firstNameGroups = firstNames.GroupBy(n => n);
+            foreach(var g in firstNameGroups.OrderByDescending(g => g.Count()).Take(20))
+            {
+                output.WriteLine($"{g.Count()} {g.Key}");
+            }
+        }
+    }
+}
